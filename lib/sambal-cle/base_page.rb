16b13ef074e1261667ce7006edfb1dfa43218e9a
Abraham Heward <aheward@rsmart.com>
2012-12-05 08:22:13 -0700

    Removed the damballa method definition from the BasePage because it's not necessary there any more.

--- lib/sambal-cle/base_page.rb
+++ lib/sambal-cle/base_page.rb
@@ -11,9 +11,9 @@ class BasePage < PageFactory
       value(:alert_box) { |b| b.frm.div(:class=>"alertMessage").text }
       # Returns the text of the header.
       value(:header) { |b| b.frm.div(:class=>"portletBody").h3.text }
-      button("Cancel")
-      button("Save")
-      button("Back")
+      button "Cancel"
+      button "Save"
+      button "Back"
     end
 
     def link(link_text)
@@ -24,13 +24,6 @@ class BasePage < PageFactory
     def button(button_text)
       element(damballa(button_text+"_button")) { |b| b.frm.button(:value=>button_text) }
       action(damballa(button_text)) { |b| b.frm.button(:value=>button_text).click }
-    end
-
-    def damballa(text)
-      text.gsub(/([+=|\\\.~@#'"\?`!\{\}\[\]\$%\^&\*\(\)])/, "").
-          gsub(/([-\/\ ])/,"_").
-          downcase.
-          to_sym
     end
 
     # Any additional needed element group defs go here...
